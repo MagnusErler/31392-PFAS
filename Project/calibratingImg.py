b6eb404976392b2930cae472de22881c32b07a1b
Magnus Erler <magnus.erler@gmail.com>
2023-04-20 11:07:35 +0200

    Updated calibrating files

--- Project/calibratingImg.py
+++ Project/calibratingImg.py
@@ -15,67 +15,85 @@ def save_coefficients(mtx, dist, path):
 
 def calibrate_camera(path, camera_calibration_file):
     # Implement the number of vertical and horizontal corners
-    nb_vertical = 5
-    nb_horizontal = 7
-    i =0
+    # nb_vertical = 7
+    # nb_horizontal = 5
+
+    # nb_vertical = 7
+    # nb_horizontal = 11
+    
+    # nb_vertical = 15
+    # nb_horizontal = 5
+    
+    dimensions = [[7,5], [7,11], [15,5]]
+    
+    i = 0
     picture_number = 0
     # Arrays to store object points and image points from all the images.
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
 
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    # Defining the world coordinates for 3D points
-    objp = np.zeros((1, nb_vertical * nb_horizontal, 3), np.float32)
-    objp[0,:,:2] = np.mgrid[0:nb_vertical, 0:nb_horizontal].T.reshape(-1, 2)
 
-    # Extracting path of individual image stored in a given directory
     images = glob.glob(path + '/*.png')
     assert images
     image_paths_sorted = sorted(images, key=lambda i: int(os.path.splitext(os.path.basename(i))[0]))
-    
-    for fname in image_paths_sorted:
 
-        # Lars' code (Path cannot inlcude numbers)
-        picture_number = int(re.sub('\D', '', str(image_paths_sorted[i])))
-        i +=1 
+    for dimension in dimensions:
+        i = 0
+        nb_vertical = dimension[0]
+        nb_horizontal = dimension[1]
+        
+        # Defining the world coordinates for 3D points
+        objp = np.zeros((1, nb_vertical * nb_horizontal, 3), np.float32)
+        objp[0,:,:2] = np.mgrid[0:nb_vertical, 0:nb_horizontal].T.reshape(-1, 2)
 
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Extracting path of individual image stored in a given directory
+        
+        for fname in image_paths_sorted:
 
-        # Implement findChessboardCorners here
-        ret, corners = cv2.findChessboardCorners(gray, (nb_vertical,nb_horizontal), cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+            # Lars' code (Path cannot include numbers)
+            picture_number = int(re.sub('\D', '', str(image_paths_sorted[i])))
+            i+=1 
 
-        # If found, add object points, image points (after refining them)
-        if ret == True:
-            objpoints.append(objp)
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            # refining pixel coordinates for given 2d points.
-            corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
-            
-            imgpoints.append(corners2)
-    
-            # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (nb_vertical,nb_horizontal), corners2, ret)
+            # Implement findChessboardCorners here
+            #ret, corners = cv2.findChessboardCorners(gray, (nb_vertical,nb_horizontal), cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+            ret, corners = cv2.findChessboardCorners(gray, (nb_vertical,nb_horizontal), None)
 
-        # Insert text into image
-        #font = cv2.FONT_HERSHEY_SIMPLEX
-        #cv2.putText(img, 'Picture {}'.format(picture_number), (10, 30), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
-        #cv2.imshow('image', img)
-        #cv2.waitKey(100)
+            # If found, add object points, image points (after refining them)
+            if ret == True:
+                objpoints.append(objp)
 
-    cv2.destroyAllWindows()
+                # refining pixel coordinates for given 2d points.
+                corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
+                
+                imgpoints.append(corners2)
+        
+                # Draw and display the corners
+                img = cv2.drawChessboardCorners(img, (nb_vertical,nb_horizontal), corners2, ret)
 
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+            # Insert text into image
+            # font = cv2.FONT_HERSHEY_SIMPLEX
+            # cv2.putText(img, 'Picture {}'.format(picture_number), (10, 30), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            # cv2.imshow('image', img)
+            # cv2.waitKey(10)
+
+        cv2.destroyAllWindows()
+
+    ret, matrix, distortion, r_vecs, t_vecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
     # Save camera matrix and distortion coefficients
     #np.savez(camera_calibration_file + ".npz", mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
-    save_coefficients(mtx, dist, camera_calibration_file + ".yaml")
+    save_coefficients(matrix, distortion, camera_calibration_file + ".yaml")
 
-    return mtx, dist
+    return matrix, distortion
 
-def undistort_image(path, mtx, dist):
+def undistort_image(path, matrix, distortion):
     # Extracting path of individual image stored in a given directory
-    images = glob.glob(path + '/*.png')
+    #images = glob.glob(path + '/*.png')
+    images = glob.glob(path + '/0000000000.png')
     assert images
     image_paths_sorted = sorted(images, key=lambda i: int(os.path.splitext(os.path.basename(i))[0]))
     print(image_paths_sorted)
@@ -84,34 +102,29 @@ def undistort_image(path, mtx, dist):
         picture_number = int(re.sub('\D', '', str(image_paths_sorted[i])))
         i+=1
         img = cv2.imread(fname)
-        h,  w = img.shape[:2]
-        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+        h, w = img.shape[:2]
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(matrix, distortion, (w,h), 1, (w,h))
 
-        # undistort
-        dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+        # undistortP=camera_matrix
+        undst = cv2.undistort(img, matrix, distortion, None, newcameramtx)
 
         # crop the image
-        x, y, w, h = roi
-        dst = dst[y:y+h, x:x+w]
-        #cv2.imwrite('calibresult.png', dst)
+        #x, y, w, h = roi
+        #undst = undst[y:y+h, x:x+w]
+        #cv2.imwrite('calibresult.png', undst)
 
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(dst, 'Picture {}'.format(picture_number), (10, 30), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(undst, 'Undistorted Image {}'.format(picture_number), (10, 30), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(img, 'Original Image {}'.format(picture_number), (10, 30), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
         # Make dst and img the same size
-        dst = cv2.resize(dst, (img.shape[1], img.shape[0]))
+        undst = cv2.resize(undst, (img.shape[1], img.shape[0]))
 
-        numpy_horizontal = np.hstack((dst, img))
-        cv2.imshow('image', numpy_horizontal)
-        #cv2.imwrite('calibresult.png', dst)
-        cv2.waitKey(100)
-
-        # # Show images side by side
-        # plt.subplot(121),plt.imshow(img)
-        # plt.subplot(122),plt.imshow(dst)
-        # plt.show()
-
-
+        # numpy_horizontal = np.hstack((undst, img))
+        # cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+        # cv2.imshow('Image', numpy_horizontal)
+        # #cv2.imwrite('calibresult.png', dst)
+        # #cv2.waitKey(0)
 
 if __name__ == '__main__':
 
@@ -120,6 +133,3 @@ if __name__ == '__main__':
 
     undistort_image("seq_01/image_L/data", mtx_L, dist_L)
     #undistort_image("seq_01/image_L/data", mtx_R, dist_R)
-
-    
- 
