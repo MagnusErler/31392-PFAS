45e4d9feafdd9b14c06a7ec1e786d922b1b2348b
Chris K <s223573@dtu.dk>
2023-05-08 13:09:10 +0200

    updated getPixelValue.py

--- Project/getPixelValue.py
+++ Project/getPixelValue.py
@@ -1,14 +1,18 @@
-from PIL import Image
+import cv2 as cv
+import numpy as np
 
 def getPixelValue(image_path, x, y):
 
-
+    img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
     if x < 0 or y < 0:
         print("Error: x or y is negative")
         return -1
-
-    #with Image.open(image_path) as img:
-        # Get the RGB value of the pixel at (x, y)
-    pixel_value = image_path.getpixel((x, y))
     
-    return pixel_value
+    kernel1 = np.ones((7,7),np.float32)/49
+    blur = cv.filter2D(img,-1,kernel1)
+
+    kernel2 = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+
+    neighborhood = blur[x-1:x+2, y-1:y+2]
+    
+    return np.sum(kernel2 * neighborhood)
